0db1dd9c145ae28a49084860d9fbb051f78f3512
Bishnu Dev <bishnu@Bishnus-MBP.domain.name>
2019-06-07 19:50:54 +0545

    fixed bugs

--- components/global/config.js
+++ components/global/config.js
@@ -1,3 +1,3 @@
 import { Platform } from 'react-native';
 
-global.DOMAIN = 'http://192.168.100.21:8000/'; // remember to add '/' at the end of the url
+global.DOMAIN = 'https://www.gomusix.net/'; // remember to add '/' at the end of the url
